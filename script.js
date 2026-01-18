ffb4c62fd3b305a6b736424d927c069b4f26f52d
GuilhermeG460 <guilhermesantosg46@gmail.com>
2026-01-18 16:21:28 -0300

    Update abrirProjeto function to open a specific URL

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function abrirProjeto(url) {
-  window.open(url, "_blank");
+function abrirProjeto(https://autocars.wuaze.com/?i=1) {
+  window.open(https://autocars.wuaze.com/?i=1, "_blank");
 }
 
   
+
